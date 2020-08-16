43efb945ee1319bac042aec39557222850f25a74
Mansiag22 <mansi22ag@gmail.com>
2020-08-16 20:04:34 +0530

    styled detail.html and home.html

--- lms/courses/views.py
+++ lms/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Course,SubCourse,Student
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def home(request):
@@ -7,6 +8,7 @@ def home(request):
     all_courses = Course.objects.raw('SELECT * FROM courses_course;')
     return render(request,'courses/home.html',{'t':all_courses})
 
+@login_required
 def getmycourses(request,errorfromcreate=None):
     print(errorfromcreate)
     student_course = Student.objects.filter(student=request.user).values()
@@ -17,6 +19,7 @@ def getmycourses(request,errorfromcreate=None):
         course_id__in=courseids).values()
     return render(request,'courses/mycourses.html',{'V':student_course_details,'num':[0,0,0]})
 
+@login_required
 def register(request,course_id):
     s = Student()
     s.student = request.user
@@ -29,6 +32,7 @@ def detail(request,course_id):
     getcourse = get_object_or_404(Course, course_id=course_id)
     return render(request,'courses/detail.html',{'course':getcourse})
 
+@login_required
 def createcourse(request):
     if request.user.is_superuser:
         if request.method=='POST':
@@ -45,12 +49,7 @@ def createcourse(request):
             subcourse1.video3 = request.FILES['video1.3']
             subcourse1.video4 = request.FILES['video1.4']
             subcourse1.transcript = request.FILES['transcript1']
-            subcourse1.quiz_marks = 0
-            if request.POST['mandatory1']=='Yes':
-                subcourse1.mandatory = 1
-            elif request.POST['mandatory1']=='No':
-                subcourse1.mandatory = 0
-            subcourse1.save()
+            subcourse1.quiz = request.POST['quiz1']
 
             subcourse2 = SubCourse()
             subcourse2.course_id = c
@@ -60,11 +59,7 @@ def createcourse(request):
             subcourse2.video3 = request.FILES['video2.3']
             subcourse2.video4 = request.FILES['video2.4']
             subcourse2.transcript = request.FILES['transcript2']
-            subcourse2.quiz_marks = 0
-            if request.POST['mandatory2'] == 'Yes':
-                subcourse2.mandatory = 1
-            elif request.POST['mandatory2'] == 'No':
-                subcourse2.mandatory = 0
+            subcourse2.quiz = request.POST['quiz2']
             
             subcourse3 = SubCourse()
             subcourse3.course_id = c
@@ -74,11 +69,7 @@ def createcourse(request):
             subcourse3.video3 = request.FILES['video3.3']
             subcourse3.video4 = request.FILES['video3.4']
             subcourse3.transcript = request.FILES['transcript3']
-            subcourse3.quiz_marks = 0
-            if request.POST['mandatory3'] == 'Yes':
-                subcourse3.mandatory = 1
-            elif request.POST['mandatory3'] == 'No':
-                subcourse3.mandatory = 0
+            subcourse3.quiz = request.POST['quiz3']
             
             subcourse4 = SubCourse()
             subcourse4.course_id = c
@@ -88,11 +79,7 @@ def createcourse(request):
             subcourse4.video3 = request.FILES['video4.3']
             subcourse4.video4 = request.FILES['video4.4']
             subcourse4.transcript = request.FILES['transcript4']
-            subcourse4.quiz_marks = 0
-            if request.POST['mandatory4'] == 'Yes':
-                subcourse4.mandatory = 1
-            elif request.POST['mandatory4'] == 'No':
-                subcourse4.mandatory = 0
+            subcourse4.quiz = request.POST['quiz4']
             
             subcourse4.save()
             subcourse2.save()
@@ -104,3 +91,16 @@ def createcourse(request):
             return render(request,'courses/createcourse.html')
     else:
         return getmycourses(request,'You are not an admin')
+
+@login_required   
+def displayallaboutcourse(request,course_id):
+    getcourse = get_object_or_404(Course, course_id=course_id)
+    getsubcourse1 = get_object_or_404(SubCourse, course_id=course_id,sub_course_no=1)
+    getsubcourse2 = get_object_or_404(SubCourse, course_id=course_id,sub_course_no=2)
+    getsubcourse3 = get_object_or_404(
+        SubCourse, course_id=course_id, sub_course_no=3)
+    getsubcourse4 = get_object_or_404(
+        SubCourse, course_id=course_id, sub_course_no=4)
+
+    return redirect('home')
+
